fb85c75ce15b491182b807a38938e41f2b34e65b
gianluca.rizzo2 <gianluca.rizzo2@sistinf.it>
2023-12-06 17:14:28 +0100

    impl

--- pages/api/form.js
+++ pages/api/form.js
@@ -68,8 +68,8 @@ export default async function handler(req, res) {
       port: 465,
       secure: true, //ssl
       auth: {
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASS
+        user:'gianlucaeannasposi@zohomail.eu',
+        pass:'Giari2.100320'
     }
     });
 
